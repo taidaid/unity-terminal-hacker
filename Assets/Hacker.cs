21a4e16dcee2d52fdc4008ff2c49c3bcd8da5999
Bryan <windsor.bryan@gmail.com>
2020-12-09 17:03:46 +0100

    initial commit, shows main menu

---
+++ Assets/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start method executes");
+        ShowMainMenu("Taidai");
+    }
+
+    // Sets the initial text to the screen to instruct user what to do
+    void ShowMainMenu(string userName)
+    {
+        print("ShowMainMenu method executes");
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Welcome " + userName + ", choose your victim\n");
+        Terminal.WriteLine("Press 1 for Hardberry Farm");
+        Terminal.WriteLine("Press 2 for KHNW News");
+        Terminal.WriteLine("Press 3 for National Security Agency\n");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
