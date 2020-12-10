8bf157052224e16864da409802af24b710824552
Bryan <windsor.bryan@gmail.com>
2020-12-10 17:04:41 +0100

    accepting passwords and looping back to main menu

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
+    private string[] level1Passwords = new string[] { "goat", "eagle", "truck", "horns", "stable" };
+    private string[] level2Passwords = new string[] { "headline", "broadcast", "anchorage", "television", "satellite"};
+    private string[] level3Passwords = new string[] { "confidential", "priveleged", "espionage", "geopolitical", "authentication" };
+
+    private string incorrectPasswordMessage = " ** Incorrect Password **";
+    private string winMessage = "You win!";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +28,7 @@ public class Hacker : MonoBehaviour
     void ShowMainMenu(string userName)
     {
         print("ShowMainMenu method executes");
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Welcome " + userName + ", choose your victim\n");
         Terminal.WriteLine("Press 1 for Hardberry Farm");
@@ -29,19 +43,88 @@ public class Hacker : MonoBehaviour
         if(input == "menu")
         {
             ShowMainMenu("Taidai");
-        } else if(input == "1")
+        } else if(currentScreen == Screen.MainMenu)
         {
-            print("1");
-        } else if(input == "2")
+            RunMainMenu(input);
+        }else if(currentScreen == Screen.Password)
         {
-            print("2");
+            CheckPassword(input);
+        }else if(currentScreen == Screen.Win)
+        {
+            ShowMainMenu("Taidai");
         }
-        else if(input == "3")
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "1")
         {
-            print("3");
-        } else
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
         {
             Terminal.WriteLine("Invalid input!");
+        }
+    }
+
+    private void RunWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        Terminal.WriteLine(winMessage);
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("Please enter your password");
+    }
+
+    void CheckPassword(string input)
+    {
+        if(level == 1)
+        {
+            if(Array.Exists(level1Passwords, el => el == input))
+            {
+                RunWinScreen();
+            }
+            else
+            {
+                Terminal.WriteLine(incorrectPasswordMessage);
+            }
+        }
+        else if(level == 2)
+        {
+            if(Array.Exists(level2Passwords, el => el == input))
+            {
+                RunWinScreen();
+            }
+            else
+            {
+                Terminal.WriteLine(incorrectPasswordMessage);
+            }
+        }
+        else if(level == 3)
+        {
+            if(Array.Exists(level3Passwords, el => el == input))
+            {
+                RunWinScreen();
+            }
+            else
+            {
+                Terminal.WriteLine(incorrectPasswordMessage);
+            }
         }
     }
 }
